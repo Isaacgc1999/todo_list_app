442175b2193ce2a6ee0a7b7681c25c05ce2c9211
Isaac <isaactraba@gmail.com>
2025-01-15 13:52:12 +0100

    home basic structure

--- src/app/components/todo-home/todo-home.component.ts
+++ src/app/components/todo-home/todo-home.component.ts
@@ -1,18 +1,17 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-home',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './todo-home.component.html',
   styleUrl: './todo-home.component.scss',
   standalone: true,
 })
-export class TodoHomeComponent implements OnInit{
+export class TodoHomeComponent{
   currentDateAndTime: string | null = null;
 
-  constructor(){}
-
-  ngOnInit(): void {
+  constructor(private datePipe: DatePipe){
+    this.currentDateAndTime = this.datePipe.transform(new Date());
   }
-
 }
