6141fc0ec55fe71d3f099d806ad1a5a8d944b788
Isaac <isaactraba@gmail.com>
2025-01-27 12:07:04 +0100

    input bar styles + main functionality

--- src/app/components/utils/todo-input-bar/todo-input-bar.component.ts
+++ src/app/components/utils/todo-input-bar/todo-input-bar.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Output } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
@@ -12,11 +18,27 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatIcon,
   ],
   standalone: true,
   templateUrl: './todo-input-bar.component.html',
   styleUrl: './todo-input-bar.component.scss',
 })
 export class TodoInputBarComponent {
-  @Output() todoFormControl = new FormControl('');
+  todoForm: FormGroup;
+  @Output() task: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      task: [''],
+    });
+  }
+
+  addTask(): void {
+    if (this.todoForm.valid) {
+      this.task = this.todoForm.value.task;
+      console.log('Task added:', this.task);
+      this.todoForm.reset();
+    }
+  }
 }
