ad5fc0f6de02348d3adebe14b68798bdc7781cdc
Isaac <isaactraba@gmail.com>
2025-02-03 13:32:54 +0100

    slashed completed tasks

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, CommonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
   standalone: true,
 })
 export class TodoListComponent {
   @Input() tasks: { id: number; taskName: string }[] = [];
+  @Input() showCompleted: boolean = false;
   @Output() taskChecked = new EventEmitter<{ id: number; taskName: string }>();
 
   constructor() {}
