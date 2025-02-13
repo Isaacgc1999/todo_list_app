dc51b63a5a84a636ce0cb7ee119d52bb71c8bb4e
Isaac <isaactraba@gmail.com>
2025-02-13 12:00:27 +0100

    unit test for darkmode component

--- src/app/components/todo-darkmode/todo-darkmode.component.spec.ts
+++ src/app/components/todo-darkmode/todo-darkmode.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { TodoDarkmodeComponent } from './todo-darkmode.component';
 
 describe('TodoDarkmodeComponent', () => {
   let component: TodoDarkmodeComponent;
   let fixture: ComponentFixture<TodoDarkmodeComponent>;
+  let bottomSheet: jasmine.SpyObj<MatBottomSheet>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoDarkmodeComponent]
-    })
-    .compileComponents();
+      imports: [TodoDarkmodeComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoDarkmodeComponent);
+    bottomSheet = TestBed.inject(
+      MatBottomSheet
+    ) as jasmine.SpyObj<MatBottomSheet>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open the bottom menu when the button is clicked', () => {
+    component.openDarkModeOps();
+    expect(bottomSheet.open).toHaveBeenCalledWith(jasmine.any(Function));
   });
 });
